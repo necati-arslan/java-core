1db694da00febfd97f8db9c5e85d3667aecaed1a
necati <necati.arslan2018@gmail.com>
2024-01-10 11:52:39 +0100

    primitive types

--- read.md
+++ read.md
@@ -4,3 +4,11 @@
   javac com/neco/Main.java
 - ##run bit code;
   src java com.neco.Main
+
+## Primitive types
+
+int myNum = 5; // Integer (whole number)
+float myFloatNum = 5.99f; // Floating point number
+char myLetter = 'D'; // Character
+boolean myBool = true; // Boolean
+String myText = "Hello"; // String
--- src/com/neco/Main.java
+++ src/com/neco/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+
     }
 }
